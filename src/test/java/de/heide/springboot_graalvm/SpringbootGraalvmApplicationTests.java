2efe7d7273474ec8fcaf1a6dff0640cff14714b5
Umair Ashraf <umair.ashraf@extern.it2media.de>
2025-09-15 23:10:45 +0200

    Init Repo

---
+++ src/test/java/de/heide/springboot_graalvm/SpringbootGraalvmApplicationTests.java
@@ -0,0 +1,13 @@
+package de.heide.springboot_graalvm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootGraalvmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
